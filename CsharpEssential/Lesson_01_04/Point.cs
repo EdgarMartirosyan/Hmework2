e6173abdd1bece39112b2a2e936b3ae82be298d3
Edgar <edgar.martirosyan.a@gmail.com>
2019-03-24 21:00:30 +0400

    2403_2059

---
+++ CsharpEssential/Lesson_01_04/Point.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_01_04
+{
+    class Point
+    {
+        private int x;
+        private int y;
+        private string name;
+
+        public int X
+        {
+            get { return x; }
+        }
+        public int Y
+        {
+            get { return y; }
+        }
+        public string Name
+        {
+            get { return name; }
+        }
+
+        public Point(string name, int x, int y)
+        {
+            this.name = name;
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
