8db678f9948ebacf8a5d33f0b59708c33c524456
EdgarMartirosyan <47984694+EdgarMartirosyan@users.noreply.github.com>
2019-03-25 14:49:22 +0400

    2503_1449

---
+++ CsharpEssential/Lesson_02_04/Invoce.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_02_04
+{
+    class Invoice
+    {
+        readonly int account;
+        readonly string customer;
+        readonly string provider;
+        public Invoice( int account, string customer,string provider)
+        {
+            this.account = account;
+            this.customer = customer;
+            this.provider = provider;
+        }
+
+        private string article;
+        private int quantity;
+    }
+}
--- CsharpEssential/Lesson_02_04/Program.cs
+++ CsharpEssential/Lesson_02_04/Program.cs
@@ -4,17 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Lesson_01_04
+namespace Lesson_02_04
 {
     class Program
     {
-
         static void Main(string[] args)
         {
-            Figure figure = new Figure(new Point("A", 2, 3), new Point("B", 3, 4), new Point("C", 4, 5));
-            figure.PrimetrCalculator();
-            Console.ReadKey();
         }
-
     }
 }
