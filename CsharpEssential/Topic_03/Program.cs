44ba659ee18a320404c63b8409c99b0a5ae9d675
EdgarMartirosyan <47984694+EdgarMartirosyan@users.noreply.github.com>
2019-04-09 17:50:29 +0400

    0904_1750

--- CsharpEssential/Topic_03/Program.cs
+++ CsharpEssential/Topic_03/Program.cs
@@ -10,6 +10,44 @@ namespace Topic_03
     {
         static void Main(string[] args)
         {
+            int[] array = new int[] { 10, 11, 12, 13, 14, 20, 60, 90, 100, 102, 105, 114, 41026 };
+            int x = 14;
+            int left = 0;
+            int right = array.Length - 1;
+            int mid = 0;
+
+            while (left <= right)
+            {
+                if (x > array[right] || x < array[left])
+                {
+                    Console.WriteLine("No");
+                    break;
+                }
+
+                mid = (left + right) / 2;
+
+                if (array[mid] > x)
+                {
+                    right = mid - 1;
+                }
+
+                else if (array[mid] < x)
+                {
+                    left = mid + 1;
+                }
+
+                else if (array[mid] == x)
+
+                {
+                    Console.WriteLine("yes");
+                    break;
+                }
+
+                if (array[mid] != x)
+
+                    Console.WriteLine("No");
+            }
+            Console.ReadKey();
         }
     }
 }
