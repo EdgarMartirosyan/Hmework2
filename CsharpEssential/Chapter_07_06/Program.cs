06b87fb4fe42179087b6a32fbdab1d3b99930874
EdgarMartirosyan <47984694+EdgarMartirosyan@users.noreply.github.com>
2019-05-17 18:27:12 +0400

    1705_1827

---
+++ CsharpEssential/Chapter_07_06/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter_07_06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 5,8,2,19,19,19,100,101,107, 28,12,12,1,3,95 };
+            int count = 1;
+            int max = 0;
+            int chek = 0;
+            
+           
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i+1; j < array.Length; j++)
+                {
+                    
+                    if (array[i]<array[j] && array[j]>chek)
+                    {
+                      // int[] trans = { array[i]};
+                           count++;
+                    }
+                    if (array[j] > chek)
+                    {
+                        chek = array[j];
+                    }
+
+                }
+                chek = 0;
+                if (count > max)
+                {
+                    max = count;
+                }
+                count = 1;
+            }
+
+            
+            Console.WriteLine(max);
+
+            Console.WriteLine(new string('-',30));
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CsharpEssential/ConsoleApp14/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int> ts = new HashSet<int>();
+            ts.Add(5);
+            ts.Add(10);
+            
+            ts.Add(20);
+            ts.Add(15);
+            foreach (int item in ts)
+            {
+                Console.WriteLine(item);
+            }
+            HashSet<int> hs2 = new HashSet<int>();
+            hs2.Add(15);
+            hs2.Add(20);
+            hs2.Add(21);
+
+            HashSet<int> hs3 = new HashSet<int>();
+            hs3.IsSubsetOf(hs2);
+            Console.WriteLine(new string('-',30));
+
+            Console.WriteLine(hs2.IsSubsetOf(ts));
+            Console.WriteLine(new string('-', 30));
+            Console.WriteLine(hs2.ToString());
+           
+            Console.ReadKey();
+        }
+    }
+}
